76cff1ceee581fd2b9e2bc1b4074f4aca8d11a25
Derek <darmfield2023@gmail.com>
2022-09-24 19:53:24 -0400

    Initial commit

--- python/tf_to_tfjs.py
+++ python/tf_to_tfjs.py
@@ -3,16 +3,7 @@ import tensorflow as tf
 from PIL import Image
 
 def convert_and_save(model, tfjs_target_dir):
-      # saves models as model.json file. os.path.join with specific 
+      print(model.summary())
       tfjs.converters.save_keras_model(model, tfjs_target_dir)
-      
-      print('-- -- -- -- MODELS CONVERTED -- -- -- --')
-
-model = tf.keras.models.load_model('savedModels/tf_ViT_covid_classifier/')
-print(model.summary())
-img = Image.open('COVID-CT/example_data/covid_ct_scan_0-14.jpg')
-resized = img.resize((224, 224))
-resized.show()
-# image_array  = tf.keras.preprocessing.image.img_to_array(resized)
-
+      print('-- -- -- -- MODEL CONVERTED -- -- -- --')
 
